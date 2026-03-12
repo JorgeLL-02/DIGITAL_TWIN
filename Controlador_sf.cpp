b19c1eba48bda74b56f5ef3173b6ea3858b8f016
Jorge <alu0101410654@ull.edu.es>
2026-03-12 16:31:26 +0000

    Se pasó el controlador a código C++ y se realizó el MIL empleando en Simulink un bloque S-function generado automaticamente al hacer "Build" en el controlador PI.

---
+++ Controlador_sf.cpp
@@ -0,0 +1,12 @@
+/*
+* Generated S-function Target for model Controlador.
+* This source file provides access to the generated S-function target
+* for other models.
+*
+* Created: Thu Mar 12 15:34:44 2026
+*/
+
+#include "Controlador_sf.h"
+#include "Controlador_sfcn_rtw\Controlador_sf.cpp"
+
+
